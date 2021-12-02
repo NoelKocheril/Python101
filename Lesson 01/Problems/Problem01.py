fb8be61baefd30a0ccbbcb201a8655ad62bcc45e
Noel Kocheril <NoelKocheril@Gmail.com>
2021-12-02 17:37:12 -0500

    The Initial Problem for Lesson 01

---
+++ Lesson 01/Problems/Problem01.py
@@ -0,0 +1,9 @@
+"""
+For this lesson I would like you to confirm that your installion of python and VS Code are all working appropraitely.
+
+1. Open Your Terminal
+2. Type in "Python3 --version" and let me know what you get back
+3. Within your VS Code Environment, create a Python File that prints out your name.
+
+Note: Printing to the Screen can be done using the command print. For example: print("Hello, World!")
+"""
