216796f7b8894cb3c89625cbbc3054d8b8f87dec
Noel Kocheril <NoelKocheril@Gmail.com>
2021-12-04 19:12:19 -0500

    Add Solution for Excerise 2.1

---
+++ Lesson 02/Solutions/Exercise2_1.py
@@ -0,0 +1,7 @@
+myName = "Noel Kocheril"
+myAge = 24
+myFavoritePlaces = ["Denny's", "My House", "The Gym"]
+
+print("My Name is" + myName)
+print("My Age is" + str(myAge))
+print("My Favorite Places is" + str(myFavoritePlaces))
