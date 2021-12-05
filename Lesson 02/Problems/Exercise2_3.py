704d9eea696b4637792c8ea4d2423ce708b71a41
Noel Kocheril <NoelKocheril@Gmail.com>
2021-12-04 22:13:47 -0500

    Created Starter File for Exercise 2.3

---
+++ Lesson 02/Problems/Exercise2_3.py
@@ -0,0 +1,17 @@
+# Exercise 2.3 Exam Attendance Checker
+#
+# A student is only allowed to attend the exam if their attendance is greater than or equal to 75%.
+#
+# Take the following inputs from the user:
+#
+# 1. The number of classes held.
+# 2. The number of classes attended.
+#
+# and print:
+#
+# 1. The percentage of classes attended.
+# 2. Whether or not the student is allowed to attend the exam.
+#
+# Note: You can get input from the user via the function input()
+
+numberOfClassesHeld = input("Enter the number of classes held: ")
