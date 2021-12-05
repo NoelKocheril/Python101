306626aee936be0e933b5bb81d7cfc7f8440e962
Noel Kocheril <NoelKocheril@Gmail.com>
2021-12-04 20:50:21 -0500

    Added Solution for Exercise 2.2

---
+++ Lesson 02/Solutions/Exercise2.2.py
@@ -0,0 +1,7 @@
+x = 5
+if x > 2:
+    print("X is greater than 2")  # Needed to Increase Identation
+    print("X is greater than 1")
+    if x < 10:
+        print("X is less than 10")
+        print("X is less than 20")  # Needed to convert tabs to spaces
