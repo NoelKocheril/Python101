b4188bdae6bdd873531facf0897a6535cec72da6
Noel Kocheril <NoelKocheril@Gmail.com>
2021-12-04 22:11:42 -0500

    Updated Exercise Starter Files

---
+++ Lesson 02/Problems/Exercise2_1.py
@@ -0,0 +1,11 @@
+# Exercise 2.1: Creating a variable and print it out
+#
+# In this exercise, I would like you to create three variables:
+#
+# 1. Your Name
+# 2. Your Age
+# 3. A list of places you like
+#
+# and print them out with a blurb before each. For example, "My Name is: {Name}".
+#
+# Note: You can convert an integer or a list to a string by using the str() function.
--- Lesson 02/Problems/Exercise2_2.py
+++ Lesson 02/Problems/Exercise2_2.py
@@ -1,3 +1,11 @@
+# Exercise 2.2 Fix the indentation
+#
+# Throughout the following code, there are multiple mistakes with how the indentation is set up. 
+# Without the use of VS Code's auto indentation correction, please correct the indentation. 
+# You can edit this file using a standard text editor like Notepad or Nano. 
+#
+# To confirm that your updates worked accordingly, run the command "python Exercise2_2.py"
+
 x = 5
 if x > 2:
 print("X is greater than 2")
